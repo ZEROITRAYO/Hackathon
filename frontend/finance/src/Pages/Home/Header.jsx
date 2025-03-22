77666380fc536c71d269e05b280107692e133f76
Prem Patel <prem.p1@ahduni.edu.in>
2025-03-22 17:02:34 +0530

    add

--- frontend/finance/src/Pages/Home/Header.jsx
+++ frontend/finance/src/Pages/Home/Header.jsx
@@ -3,7 +3,7 @@ import Button from "../../components/Button";
 
 const Header = () => {
   return (
-    <header className="flex flex-wrap justify-between w-full whitespace-nowrap max-md:max-w-full md:max-h-full">
+    <header className="flex flex-wrap justify-between w-full whitespace-nowrap max-md:max-w-full md:max-h-full bg-transparent z-10 fixed top-0">
       <div className="flex gap-1.5 self-start mt-2.5 text-3xl text-white">
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/042223570789192f866d38f215efce3b4d958a48?placeholderIfAbsent=true&apiKey=6b1c9c0a5e6d416dbf10a19cc8dd94dd"
--- frontend/finance/src/Pages/Home/Home.jsx
+++ frontend/finance/src/Pages/Home/Home.jsx
@@ -5,19 +5,19 @@ import HeroSection from "./Hero";
 const DashboardPage = () => {
   return (
     <main >
-         
       <div className="flex relative flex-col items-center px-20 pt-24 pb-56 w-full md:h-full max-md:px-5 max-md:pb-24 max-md:max-w-full">
+        <Header />
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/21412f73d7fff4d82cfdebac446d97f36084d66b?placeholderIfAbsent=true&apiKey=6b1c9c0a5e6d416dbf10a19cc8dd94dd"
           alt="Background"
-          className="object-cover absolute inset-0 size-full"
+          className="object-cover absolute inset-0 size-full rounded-none"
         />
-        <Header className=''/>
+
         <div className="flex relative px-0 justify-center">
-       
+
         </div>
         <div className="flex relative flex-col -mb-11 w-full max-w-[1115px] max-md:mb-2.5 max-md:max-w-full max-md:max-h-full">
-        
+
           <HeroSection />
         </div>
       </div>
